5b130bd5bd393ce17650cede2cf498c5ab615f75
pacoorozco <paco@pacoorozco.info>
2019-02-02 18:48:14 +0100

    Day 1 Part 2
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

---
+++ 2018/day-01/Part 2/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("day-01-input.txt")
+	if err != nil {
+		log.Fatalf("could not read input data from file, err=%v", err)
+	}
+	defer file.Close()
+
+	var nums []int
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		change, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatalf("could not convert line to integer, err=%v", err)
+		}
+		nums = append(nums, change)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("could not read line on frequencies, err=%v", err)
+	}
+
+	freq := 0
+	seen := map[int]bool{0: true}
+
+	for {
+		for _, v := range nums {
+			freq += v
+			if seen[freq] {
+				fmt.Println(freq)
+				return
+			}
+			seen[freq] = true
+		}
+	}
+}
